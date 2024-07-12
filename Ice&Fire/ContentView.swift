d2f1d5a1632efbf1eed6fbda89555af3670a9dfa
Salman Mhaskar <salmanmhaskar17@gmail.com>
2024-07-12 21:42:08 +0530

    Scroll Transition Added

--- Ice&Fire/ContentView.swift
+++ Ice&Fire/ContentView.swift
@@ -10,21 +10,60 @@ import SwiftUI
 struct ContentView: View {
     @StateObject var viewModel = ApiHandler()
     var body: some View {
-        ScrollView{
-            VStack (spacing: 10){
-                ForEach(viewModel.characters){ character in
-                    if character.family == "House Stark"{
-                        CharacterCardView(imageUrl: character.imageUrl, firstName: character.firstName, lastName: character.lastName)
+        ZStack{
+            
+            
+            LinearGradient(colors: [.white,.gray], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            
+            Image("Starks")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 250, height: 250)
+            
+            
+            VStack{
+                Spacer()
+                
+                Text("House Stark")
+                    .bold()
+                    .font(.largeTitle)
+                    .padding()
+                
+                ScrollView(.vertical , showsIndicators: false){
+                        ForEach(viewModel.characters){ character in
+                            if character.family == "House Stark"{
+                                ZStack{
+                                    CharacterCardView(imageUrl: character.imageUrl, firstName: character.firstName, lastName: character.lastName)
+                                        .scrollTransition(axis: .vertical){ content, phase in
+                                            return content
+                                                .scaleEffect(phase.isIdentity ? 1 : 0, anchor: .topTrailing)
+                                                .opacity(phase.isIdentity ? 1 : 0)
+
+                                                
+                                                
+                                            
+                                        }
+                                }
+                                .containerRelativeFrame(.vertical)
+                                                    .clipShape(RoundedRectangle(cornerRadius: 20 ))
+                            }
+                        }
                     }
+                .scrollContentBackground(.hidden)
+                    .padding()
+                
+                Spacer()
+                
                 }
+                
+                .navigationTitle("Characters")
+                .onAppear{
+                    viewModel.fetchApi()
             }
-            .padding()
-        }
-        .navigationTitle("Characters")
-        .onAppear{
-            viewModel.fetchApi()
         }
     }
+    
 }
 
 #Preview {
