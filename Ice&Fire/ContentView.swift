2bf6f4ae8f9644ad419e6ca6ec90dc5c5309d1cc
Salman Mhaskar <salmanmhaskar17@gmail.com>
2024-07-12 19:24:31 +0530

    Initial Commit

---
+++ Ice&Fire/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ice&Fire
+//
+//  Created by Salman Mhaskar on 12/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ice&Fire/Ice_FireApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ice_FireApp.swift
+//  Ice&Fire
+//
+//  Created by Salman Mhaskar on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Ice_FireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
